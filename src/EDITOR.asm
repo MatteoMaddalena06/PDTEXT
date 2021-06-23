e28cb91505d487ee747447e09e54ae14d068d7b4
Mattstack_ <madomspa@hotmail.com>
2021-06-23 19:53:16 +0200

    Update EDITOR.asm

--- src/EDITOR.asm
+++ src/EDITOR.asm
@@ -26,13 +26,13 @@
 procedure SEGMENT PARA PUBLIC
 
     ;-------------------------------------------
-	; Init the editor:
-	;   Set the video mode (with BIOS Service)
-	;   Print the menu (using MMIO VRAM)
-	;   Set cursor position (with BIOS service)
-	;
-	; Parameters: NULL
-	; Value return: NULL
+    ; Init the editor:
+    ;   Set the video mode (with BIOS Service)
+    ;   Print the menu (using MMIO VRAM)
+    ;   Set cursor position (with BIOS service)
+    ;
+    ; Parameters: NULL
+    ; Value return: NULL
 		
 		init_editor PROC FAR
 			
@@ -47,23 +47,23 @@ procedure SEGMENT PARA PUBLIC
 			push	ds
 
 			;vide mode 03h (BIOS service)
-			mov		ah, 0h
-			mov		al, 03h
-			int		10h
+			mov	ah, 0h
+			mov	al, 03h
+			int	10h
 
 			;set cursor position (BIOS service)
-			mov		ah, 02h
+			mov	ah, 02h
 			xor     bh, bh
-			mov		dh, MIN_EDITOR_LINE
-			mov		dl, MIN_EDITOR_COLUMN
+			mov	dh, MIN_EDITOR_LINE
+			mov	dl, MIN_EDITOR_COLUMN
 			int 	10h
 
 			;print window (VRAM)
-			mov		ax, menu_img
-			mov		ds, ax
-			mov		cx, 0FFFh
-			mov		si, OFFSET data_IMG
-			xor		di, di
+			mov	ax, menu_img
+			mov	ds, ax
+			mov	cx, 0FFFh
+			mov	si, OFFSET data_IMG
+			xor	di, di
 			rep    	movsw
 
 			;print file name
@@ -74,31 +74,31 @@ procedure SEGMENT PARA PUBLIC
 
 			write: mov     al, BYTE PTR [si]
 			       cmp     al, 0Dh 
-				   je      _end_e
-				   mov     ah, 0Fh 
-				   mov     WORD PTR es:[di], ax
-				   inc     si
-				   add     di, 2h
-				   loop    write
+			       je      _end_e
+			       mov     ah, 0Fh 
+			       mov     WORD PTR es:[di], ax
+			       inc     si
+			       add     di, 2h
+			       loop    write
 
 			_end_e: pop     bp
-					pop 	dx
-					pop		cx
-					pop		bx
-					pop		ax
-					ret
+				pop 	dx
+				pop	cx
+				pop	bx
+				pop	ax
+				ret
 
 		init_editor ENDP
 
     ;-------------------------------------------
 
     ;-------------------------------------------
-	; Exit to text editor:
-	;	Print exit message
-	;   Set new cursor position
-	;
-	; Prameters: NULL
-	; Value return: NULL
+    ; Exit to text editor:
+    ;	Print exit message
+    ;   Set new cursor position
+    ;
+    ; Prameters: NULL
+    ; Value return: NULL
 
 		exit_work PROC FAR
 
@@ -108,24 +108,24 @@ procedure SEGMENT PARA PUBLIC
 			push	di
 
 			;print exit text
-			mov		cx, 0FFFh
-			mov		ax, exit_msg
-			mov		ds, ax
-			xor		di, di
-			mov		si, OFFSET exit_IMG
+			mov	cx, 0FFFh
+			mov	ax, exit_msg
+			mov	ds, ax
+			xor	di, di
+			mov	si, OFFSET exit_IMG
 			rep     movsw
 
 			;set cursor position (BIOS service)
-			mov		ah, 02h
+			mov	ah, 02h
 			xor     bh, bh
-			mov		dh, 18h
-			mov		dl, 4Fh
-			int		10h
+			mov	dh, 18h
+			mov	dl, 4Fh
+			int	10h
 
-			pop		di
+			pop	di
 			pop 	ds
-			pop		bx
-			pop		ax
+			pop	bx
+			pop	ax
 			ret
 
 		exit_work ENDP
